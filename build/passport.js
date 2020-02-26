18d508f78e88a991166e63d626b8b2b67ac526a7
insungsong <song22861@naver.com>
2020-02-26 19:36:22 +0900

    suceess

--- build/passport.js
+++ build/passport.js
@@ -12,9 +12,7 @@ var _userController = require("./controllers/userController");
 
 var _routes = _interopRequireDefault(require("./routes"));
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // User.js에서는 passport를 mongoose를 통해 방법론을 쓸거야를 선언한것이라면,
 // 해당 파일에서는
@@ -23,33 +21,23 @@ function _interopRequireDefault(obj) {
 _passport.default.use(_User.default.createStrategy()); // User(passport-local-mongoose)에 새로운 로그인 방식을 알려줄거야!
 // passport에게 github사용하라고 하기
 
-_passport.default.use(
-  new _passportGithub.default(
-    {
-      clientID: process.env.GH_ID,
-      clientSecret: process.env.GH_SECRET,
-      callbackURL: process.env.NODE_ENV
-        ? `https://fathomless-bastion-84075.herokuapp.com${_routes.default.githubCallback}`
-        : `http://localhost:4000${_routes.default.githubCallback}`
-    },
-    _userController.githubLoginCallback
-  )
-);
 
-_passport.default.use(
-  new _passportFacebook.default(
-    {
-      clientID: process.env.FB_ID,
-      clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://70784bec.ngrok.io${_routes.default.facebookCallback}`,
-      profileFields: ["id", "displayName", "photos", "email"],
-      scope: ["public_profile", "email"]
-    },
-    _userController.facebookLoginCallback
-  )
-);
+_passport.default.use(new _passportGithub.default({
+  clientID: process.env.GH_ID,
+  clientSecret: process.env.GH_SECRET,
+  callbackURL: process.env.NODE_ENV ? `https://fathomless-bastion-84075.herokuapp.com${_routes.default.githubCallback}` : `http://localhost:4000${_routes.default.githubCallback}`
+}, _userController.githubLoginCallback));
+
+_passport.default.use(new _passportFacebook.default({
+  clientID: process.env.FB_ID,
+  clientSecret: process.env.FB_SECRET,
+  callbackURL: `https://70784bec.ngrok.io${_routes.default.facebookCallback}`,
+  profileFields: ["id", "displayName", "photos", "email"],
+  scope: ["public_profile", "email"]
+}, _userController.facebookLoginCallback));
 
 _passport.default.serializeUser(_User.default.serializeUser()); // 쿠키에 id를 주고
+
 
 _passport.default.deserializeUser(_User.default.deserializeUser()); // 준 id를 사용자로 전환하는 로직
 // 여기까지가 사용자 인증을 처리한것이다 어떤 방식으로 처리할지를 말이다.
